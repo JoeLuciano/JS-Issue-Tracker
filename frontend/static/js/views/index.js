24436b42ede7cbe02fd7aae1f98a97766212a1fe
JoeLuciano <joeluciano@outlook.com>
2021-02-12 16:41:53 -0500

    Dank you

---
+++ frontend/static/js/views/index.js
@@ -0,0 +1,51 @@
+import Dashboard from "./Dashboard.js";
+import Posts from "./Posts.js";
+import Settings from "./Settings.js";
+
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+}
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: Dashboard },
+        { path: "/posts", view: Posts },
+        { path: "/settings", view: Settings }
+    ];
+
+    //Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            isMatch: location.pathname === route.path
+        }
+    })
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch)
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        }
+    }
+
+    const view = new match.route.view();
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", event => {
+        if (event.target.matches("[data-link]")) {
+            event.preventDefault();
+            navigateTo(event.target.href);
+        }
+    })
+
+    router();
+});
